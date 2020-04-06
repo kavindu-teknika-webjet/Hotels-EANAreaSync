90886d7dda1de795e2a18e43541c4aa01c88cdc6
kavinduteknika <kavindu.teknika@webjet.com.au>
2020-04-06 14:32:02 +1000

    .Net core console project template

---
+++ Hotels-EANAreaSync/AreaSync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotels_EANAreaSync
+{
+    public class AreaSync
+    {
+
+        public AreaSync()
+        {}
+
+        public void Start() {
+            Console.WriteLine("Area sync is processing.");
+        }
+    }
+}
---
+++ Hotels-EANAreaSync/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hotels_EANAreaSync
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hotels area sync started.");
+            AreaSync areaSync = new AreaSync();
+            areaSync.Start();
+            Console.WriteLine("Hotels area sync finished.");
+        }
+    }
+}
